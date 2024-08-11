8bf36fd0bbb0dacc61098ca343d6ac96b8e2f73a
Suraj Kumar <surajraj4747@gmail.com>
2024-08-11 16:07:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const changeColorButton = document.getElementById('changeColorButton');
+
+
+changeColorButton.addEventListener('click', function() {
+   
+    document.body.style.backgroundColor = getRandomColor();
+});
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
